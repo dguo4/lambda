e42c2321955c330044808705673fcc7e06a31a47
dt.guo17@gmail.com <wzdyhgzl@20w>
2023-10-30 14:24:32 -0400

    switch to aws dynamoDB

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from mangum import Mangum
-from pymongo import MongoClient
-
-conn = MongoClient("mongodb+srv://miaHappy:6adL7Gi3VA7DNndf@cluster0.pks73ha.mongodb.net/?retryWrites=true&w=majority")
+import boto3
+import uvicorn
 
 app = FastAPI()
 handler = Mangum(app)
-
-def transactionEntity(item) -> dict:
-    return {
-        "id": str(item['_id']),
-        "date": item['date'],
-        "account": item['account'],
-        "ticker": item['ticker'],
-        "price": item['price'],
-        "quantity": item['quantity'],
-        "assetGroup": item['assetGroup'],
-        "assetType": item['assetType'],
-        "type": item['type']
-    }
-
-def transactionsEntity(entity) -> list:
-    return [transactionEntity(item) for item in entity]
 
 @app.get("/")
 async def my_app():
@@ -33,4 +16,13 @@ async def my_test():
 
 @app.get("/all_transactions")
 async def get_all_transactions():
-    return transactionsEntity(list(conn.investment.transactions.find()))
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('invest_transactions')
+    response = table.scan()
+    data = response['Items']
+
+    return data
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
